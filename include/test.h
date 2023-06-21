4e02771befc4d1c034698bc6f852f1a36eea65ae
Thomas Nipen <tnipen@gmail.com>
2023-06-21 13:42:37 +0200

    Initial add

---
+++ include/test.h
@@ -0,0 +1,8 @@
+#ifndef TEST_H
+#define TEST_H
+
+namespace test {
+    int func(float x);
+}
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include "test.h"
+#include <armadillo>
+
+namespace {
+    typedef arma::mat mattype;
+    typedef arma::vec vectype;
+}
+int test::func(float x) {
+    arma::vec q;
+
+    return x + 1;
+}
