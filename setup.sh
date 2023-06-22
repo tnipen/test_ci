112e10ad2e4f8edd8a27bdd392fe59c8c21092a3
Thomas Nipen <tnipen@gmail.com>
2023-06-22 13:52:22 +0200

    Copy files to root

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@ cmake ..
 VERBOSE=1 make package-python
 ls swig
 ls swig/python-packaging
+for filename in setup.py gridpp.py gridppPYTHON_wrap.cxx; do
+    cp swig/python-packaging/$filename .
+done
+ls
