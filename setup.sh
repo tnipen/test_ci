476e4bcbb024d6a319e804912ae5f0815293e043
Thomas Nipen <tnipen@gmail.com>
2023-06-22 14:10:35 +0200

    Remove gridpp files in root

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@ mkdir build
 cd build
 cmake ..
 VERBOSE=1 make package-python
-ls swig
+echo "LIST swig/python-packaging"
 ls swig/python-packaging
 for filename in setup.py gridpp.py gridppPYTHON_wrap.cxx; do
     cp swig/python-packaging/$filename .
 done
+echo "LIST HOME"
 ls
