8595cebe3f8065143bb98b9cd50ae668cd795ec8
Thomas Nipen <tnipen@gmail.com>
2023-06-22 15:11:44 +0200

    Copy numpy headers

---
+++ swig/python-packaging/setup.sh
@@ -0,0 +1,8 @@
+mkdir build
+cd build
+cmake ..
+VERBOSE=1 make package-python
+wget https://files.pythonhosted.org/packages/a5/16/8a678404411842fe02d780b5f0a676ff4d79cd58f0f22acddab1b392e230/numpy-1.12.1.zip
+unzip numpy-1.12.1.zip
+cp -r numpy-1.12.1/numpy/core/include/numpy/ include
+rm -rf numpy-1.12.1*
